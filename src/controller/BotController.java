e58aa20c98322917f9e5234c0ba82be04fca6c8b
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-03-22 13:55:18 -0300

    :rocket: Melhora lógica dos bots

--- src/controller/BotController.java
+++ src/controller/BotController.java
@@ -1,9 +1,8 @@
 package controller;
 
-import model.Pos;
-import view.GameViewer;
-
 import java.util.Random;
+
+import model.Pos;
 
 public class BotController {
 
@@ -21,18 +20,14 @@ public class BotController {
         Pos pos = this.justWin();
 
         if (pos != null) {
-            System.out.println("Win por justWin");
             return pos;
         }
 
         pos = this.justDontLose();
 
         if (pos != null) {
-            System.out.println("Win por justDontLose");
             return pos;
         }
-
-        System.out.println("Random");
 
         return this.justGetARandomValidPosition();
     }
@@ -73,7 +68,7 @@ public class BotController {
                     return new Pos(x,0);
                 } else if (this.gameController.chest[x][1]==GameController.NO_PLAYER) {
                     return new Pos(x,1);
-                } else {
+                } else if (this.gameController.chest[x][2]==GameController.NO_PLAYER) {
                     return new Pos(x,2);
                 }
             }
@@ -86,7 +81,7 @@ public class BotController {
                 return new Pos(0,0);
             } else if (this.gameController.chest[0][1]==GameController.NO_PLAYER) {
                 return new Pos(1,0);
-            } else {
+            } else if (this.gameController.chest[0][2]==GameController.NO_PLAYER) {
                 return new Pos(2,0);
             }
         }
@@ -96,7 +91,7 @@ public class BotController {
                 return new Pos(0,1);
             } else if (this.gameController.chest[1][1]==GameController.NO_PLAYER) {
                 return new Pos(1,1);
-            } else {
+            } else if (this.gameController.chest[1][2]==GameController.NO_PLAYER) {
                 return new Pos(2,1);
             }
         }
@@ -106,7 +101,7 @@ public class BotController {
                 return new Pos(0,2);
             } else if (this.gameController.chest[2][1]==GameController.NO_PLAYER) {
                 return new Pos(1,2);
-            } else {
+            } else if (this.gameController.chest[2][2]==GameController.NO_PLAYER) {
                 return new Pos(2,2);
             }
         }
@@ -116,7 +111,7 @@ public class BotController {
                 return new Pos(0,0);
             } else if (this.gameController.chest[1][1] == GameController.NO_PLAYER) {
                 return new Pos(1,1);
-            } else {
+            } else if (this.gameController.chest[2][2]==GameController.NO_PLAYER) {
                 return new Pos(2,2);
             }
         }
@@ -126,7 +121,7 @@ public class BotController {
                 return new Pos(0,2);
             } else if (this.gameController.chest[1][1] == GameController.NO_PLAYER) {
                 return new Pos(1,1);
-            } else {
+            } else if (this.gameController.chest[2][0]==GameController.NO_PLAYER) {
                 return new Pos(2,0);
             }
         }
@@ -135,6 +130,98 @@ public class BotController {
     }
 
     private Pos justDontLose() {
+        int countOnRows = 0;
+        int countOnColumn1 = 0;
+        int countOnColumn2 = 0;
+        int countOnColumn3 = 0;
+        int countOnDioganal = 0;
+        int countOnInverseDioganal = 0;
+
+        for (int x=0; x<3; x++) {
+            for (int y=0; y<3; y++) {
+                if (this.gameController.chest[x][y] == this.playerNumber) {
+                    countOnRows++;
+
+                    if (x==0) {
+                        countOnColumn1++;
+                    } else if (x==1) {
+                        countOnColumn2++;
+                    } else {
+                        countOnColumn3++;
+                    }
+
+                    if (x==y) {
+                        countOnDioganal++;
+                    }
+
+                    if (x + y == (GameController.CHEST_TAM - 1) ) {
+                        countOnInverseDioganal++;
+                    }
+                }
+            }
+
+            if (countOnRows == 2) {
+                if (this.gameController.chest[x][0]==GameController.NO_PLAYER) {
+                    return new Pos(x,0);
+                } else if (this.gameController.chest[x][1]==GameController.NO_PLAYER) {
+                    return new Pos(x,1);
+                } else if (this.gameController.chest[x][2]==GameController.NO_PLAYER) {
+                    return new Pos(x,2);
+                }
+            }
+
+            countOnRows = 0;
+        }
+
+        if (countOnColumn1 == 2) {
+            if (this.gameController.chest[0][0]==GameController.NO_PLAYER) {
+                return new Pos(0,0);
+            } else if (this.gameController.chest[0][1]==GameController.NO_PLAYER) {
+                return new Pos(1,0);
+            } else if (this.gameController.chest[0][2]==GameController.NO_PLAYER){
+                return new Pos(2,0);
+            }
+        }
+
+        if (countOnColumn2 == 2) {
+            if (this.gameController.chest[1][0]==GameController.NO_PLAYER) {
+                return new Pos(0,1);
+            } else if (this.gameController.chest[1][1]==GameController.NO_PLAYER) {
+                return new Pos(1,1);
+            } else if (this.gameController.chest[1][2]==GameController.NO_PLAYER) {
+                return new Pos(2,1);
+            }
+        }
+
+        if (countOnColumn3 == 2) {
+            if (this.gameController.chest[2][0]==GameController.NO_PLAYER) {
+                return new Pos(0,2);
+            } else if (this.gameController.chest[2][1]==GameController.NO_PLAYER) {
+                return new Pos(1,2);
+            } else if (this.gameController.chest[2][2]==GameController.NO_PLAYER) {
+                return new Pos(2,2);
+            }
+        }
+
+        if (countOnDioganal == 2) {
+            if (this.gameController.chest[0][0] == GameController.NO_PLAYER) {
+                return new Pos(0,0);
+            } else if (this.gameController.chest[1][1] == GameController.NO_PLAYER) {
+                return new Pos(1,1);
+            } else if (this.gameController.chest[2][2]==GameController.NO_PLAYER) {
+                return new Pos(2,2);
+            }
+        }
+
+        if (countOnInverseDioganal == 2) {
+            if (this.gameController.chest[0][2] == GameController.NO_PLAYER) {
+                return new Pos(0,2);
+            } else if (this.gameController.chest[1][1] == GameController.NO_PLAYER) {
+                return new Pos(1,1);
+            } else if (this.gameController.chest[2][0]==GameController.NO_PLAYER) {
+                return new Pos(2,0);
+            }
+        }
 
         return null;
     }
@@ -146,10 +233,17 @@ public class BotController {
             int x = random.nextInt(GameController.CHEST_TAM);
             int y = random.nextInt(GameController.CHEST_TAM);
 
-            System.out.println(x + " " + y);
-            if (this.gameController.chest[x][y] == GameController.NO_PLAYER) {
+            if (!isOcuppedPosition(x,y)) {
                 return new Pos(x,y);
             }
         } while(true);
+    }
+
+    private boolean isOcuppedPosition(int x, int y) {
+        if (this.gameController.chest[x][y] == GameController.NO_PLAYER) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
